4c346002c4f58b594661357ae8ff003f47415f83
Daniel <danielga@outlook.pt>
2015-05-17 16:22:12 +0100

    GetBinaryFileName wasn't working because std::string would be destroyed before it reached the factory loader. Added FactoryLoader class that should do all CDllDemandLoader does and more. Requires SourceSDK and may create multiple copies of itself if included multiple times.

--- include/interfaces.hpp
+++ include/interfaces.hpp
@@ -1,32 +1,66 @@
 #pragma once
 
+#include <interface.h>
 #include <string>
 
 namespace SourceSDK
 {
 
-inline std::string GetBinaryFileName( const std::string &name, bool serverside = false )
+class FactoryLoader
 {
+public:
+	FactoryLoader( const std::string &name, bool serverside = true, bool noload = true ) :
+		module( nullptr ),
+		factory( nullptr )
+	{
+		std::string filename = name;
 
 #if defined _WIN32
 
-	(void)serverside;
-	return name + ".dll";
+		(void)serverside;
+		filename += ".dll";
 
 #elif defined __linux
 
-	if( serverside )
-		return name + "_srv.so";
-	else
-		return name + ".so";
+		filename += serverside ? "_srv.so" : ".so";
 
 #elif defined __APPLE__
 
-	(void)serverside;
-	return name + ".dylib";
+		(void)serverside;
+		filename += ".dylib";
 
 #endif
 
-}
+		module = Sys_LoadModule( filename.c_str( ), noload ? SYS_NOLOAD : SYS_NOFLAGS );
+		if( module != nullptr )
+			factory = Sys_GetFactory( module );
+	}
+
+	~FactoryLoader( )
+	{
+		if( module != nullptr )
+			Sys_UnloadModule( module );
+	}
+
+	bool IsValid( ) const
+	{
+		return factory != nullptr;
+	}
+
+	CreateInterfaceFn GetFactory( ) const
+	{
+		return factory;
+	}
+
+	template<class Interface>
+	Interface *GetInterface( const char *name ) const
+	{
+		return IsValid( ) ? static_cast<Interface *>( factory( name, nullptr ) ) : nullptr;
+	}
+
+private:
+	CSysModule *module;
+	CreateInterfaceFn factory;
+};
 
 }
