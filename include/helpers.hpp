13e467aab0c3c815200d88235165385da9ab8363
Daniel <danielga@outlook.pt>
2016-01-30 20:13:51 +0000

    Added LuaErrorTraceback function to helpers.
    This function simulates Garry's Mod error traceback, without the extra newlines on each end of the string.

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
+#include <sstream>
+#include <lua.hpp>
 
 namespace helpers
 {
@@ -31,6 +34,40 @@ inline std::string GetBinaryFileName(
 
 #endif
 
+}
+
+static int32_t LuaErrorTraceback( lua_State *state )
+{
+	std::string spaces( "\n  " );
+	std::ostringstream stream;
+
+	if( lua_type( state, 1 ) == LUA_TSTRING )
+		stream << lua_tostring( state, 1 );
+
+	lua_Debug dbg = { 0 };
+	for(
+		int32_t lvl = 1;
+		lua_getstack( state, lvl, &dbg ) == 1;
+		++lvl, std::memset( &dbg, 0, sizeof( dbg ) )
+	)
+	{
+		if( lua_getinfo( state, "Sln", &dbg ) == 0 )
+			break;
+
+		stream
+			<< spaces
+			<< lvl
+			<< ". "
+			<< ( dbg.name == nullptr ? "unknown" : dbg.name )
+			<< " - "
+			<< dbg.short_src
+			<< ':'
+			<< dbg.currentline;
+		spaces += ' ';
+	}
+
+	lua_pushstring( state, stream.str( ).c_str( ) );
+	return 1;
 }
 
 }
