cf6a8a7b246dd175da1a3f5030c5edeb9097e36f
Daniel <danielga@outlook.pt>
2016-01-30 22:04:09 +0000

    Added PushHookRun and CallHookRun functions to helpers.
    These functions help with calling hooks and use LuaErrorTraceback as the error traceback function.

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <lua.hpp>
+#include <GarrysMod/Lua/LuaInterface.h>
 
 namespace helpers
 {
@@ -68,6 +69,58 @@ static int32_t LuaErrorTraceback( lua_State *state )
 
 	lua_pushstring( state, stream.str( ).c_str( ) );
 	return 1;
+}
+
+static bool PushHookRun( GarrysMod::Lua::ILuaBase *lua, const char *hook_name )
+{
+	lua->PushCFunction( LuaErrorTraceback );
+
+	lua->GetField( GarrysMod::Lua::INDEX_GLOBAL, "hook" );
+	if( !lua->IsType( -1, GarrysMod::Lua::Type::TABLE ) )
+	{
+		lua->Pop( 2 );
+		return false;
+	}
+
+	lua->GetField( -1, "Run" );
+	if( !lua->IsType( -1, GarrysMod::Lua::Type::FUNCTION ) )
+	{
+		lua->Pop( 3 );
+		return false;
+	}
+
+	lua->Remove( -2 );
+
+	lua->PushString( hook_name );
+
+	return true;
+}
+
+static bool CallHookRun(
+	GarrysMod::Lua::ILuaBase *lua,
+	int32_t args = 0,
+	int32_t rets = 0,
+	bool print_error = true,
+	bool pop_error = true
+)
+{
+	if( lua->PCall( 1 + args, rets, -args - 3 ) != 0 )	/* -args for the last arg, -1 for hook name,
+														-1 for hook.Run and -1 for debug.traceback */
+	{
+		if( print_error )
+			static_cast<GarrysMod::Lua::ILuaInterface *>( lua )->ErrorNoHalt(
+				"\n%s\n\n",
+				lua->GetString( -1 )
+			);
+		if( pop_error )
+			lua->Pop( 2 ); /* pop error string and debug.traceback */
+		else
+			lua->Remove( -2 ); /* remove debug.traceback */
+		return false;
+	}
+
+	lua->Remove( -rets - 1 ); /* remove debug.traceback and leave the returns in the stack */
+	return true;
 }
 
 }
