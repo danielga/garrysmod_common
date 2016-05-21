69e8cb0b7db4b9a6caa00687373ffbe64b298248
Daniel <danielga@outlook.pt>
2016-05-22 00:09:13 +0100

    Removed the GMMODULE requirement on Garry's Mod Lua headers.
    Left the GMMODULE define on premake though, to maintain compatibility
    with other stuff.

--- include/GarrysMod/Lua/Interface.h
+++ include/GarrysMod/Lua/Interface.h
@@ -4,30 +4,26 @@
 #include "LuaBase.h"
 #include "UserData.h"
 
-#if defined GMMODULE
+struct lua_State
+{
+	unsigned char _ignore_this_common_lua_header_[69];
+	GarrysMod::Lua::ILuaBase *luabase;
+};
 
-	struct lua_State
-	{
-		unsigned char _ignore_this_common_lua_header_[69];
-		GarrysMod::Lua::ILuaBase *luabase;
-	};
+#if defined _WIN32
 
-	#if defined _WIN32
+	#define GMOD_DLL_EXPORT extern "C" __declspec(dllexport)
 
-		#define GMOD_DLL_EXPORT extern "C" __declspec(dllexport)
+#else
 
-	#else
-
-		#define GMOD_DLL_EXPORT extern "C" __attribute__((visibility("default")))	
-
-	#endif
-
-	#define GMOD_MODULE_OPEN( ) GMOD_DLL_EXPORT int gmod13_open( lua_State *state )
-	#define GMOD_MODULE_CLOSE( ) GMOD_DLL_EXPORT int gmod13_close( lua_State *state )
-
-	#define LUA state->luabase
-
-	#define LUA_FUNCTION( name ) int name( lua_State *state )
-	#define LUA_FUNCTION_STATIC( name ) static LUA_FUNCTION( name )
+	#define GMOD_DLL_EXPORT extern "C" __attribute__((visibility("default")))
 
 #endif
+
+#define GMOD_MODULE_OPEN( ) GMOD_DLL_EXPORT int gmod13_open( lua_State *state )
+#define GMOD_MODULE_CLOSE( ) GMOD_DLL_EXPORT int gmod13_close( lua_State *state )
+
+#define LUA state->luabase
+
+#define LUA_FUNCTION( name ) int name( lua_State *state )
+#define LUA_FUNCTION_STATIC( name ) static LUA_FUNCTION( name )
