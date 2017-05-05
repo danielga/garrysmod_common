95966906b706fc4d5ca89db2553afda54f68a1f6
Daniel <danielga@outlook.pt>
2017-05-05 14:33:53 +0100

    Fixes #3, os.is and os.get deprecated.

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 -- These default paths are relative to the premake5.lua file (or whatever you named it) of your project
 -- They can be absolute paths too if you so desire
 -- Default path for the workspace files
-DEFAULT_WORKSPACE_DIRECTORY = os.get() .. "/" .. _ACTION
+DEFAULT_WORKSPACE_DIRECTORY = os.target() .. "/" .. _ACTION
 
 -- Default path for the source files (beware this will be used by all projects)
 DEFAULT_SOURCE_DIRECTORY = "../source"
