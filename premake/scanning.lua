2db04546f2c0795a2e8815cca8c72bee43c32d6a
Daniel <danielga@outlook.pt>
2017-05-15 22:19:00 +0100

    Updated scanning submodule and added disassembler package.

--- premake/scanning.lua
+++ premake/scanning.lua
@@ -23,3 +23,30 @@ function IncludeScanning()
 
 	filter({})
 end
+
+function IncludeDisassembler(full)
+	IncludePackage("disassembler")
+
+	local directory = _GARRYSMOD_COMMON_DIRECTORY .. "/scanning/distorm"
+
+	filter({})
+
+	if full then
+		defines("DISTORM_STATIC")
+	else
+		defines({"DISTORM_STATIC", "DISTORM_LIGHT"})
+	end
+
+	includedirs(directory .. "/include")
+	files({
+		directory .. "/include/*.h",
+		directory .. "/src/*.h",
+		directory .. "/src/*.c"
+	})
+	vpaths({
+		["Header files/distorm"] = {directory .. "/include/*.h", directory .. "/src/*.h"},
+		["Source files/distorm"] = directory .. "/src/*.c"
+	})
+
+	filter({})
+end
