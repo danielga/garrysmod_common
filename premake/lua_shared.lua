207c6437874e6d4fccbccb420fe1719d3b7693e6
Daniel <danielga@outlook.pt>
2017-08-29 22:46:16 +0100

    Removed unnecessary library directories on lua_shared.lua

--- premake/lua_shared.lua
+++ premake/lua_shared.lua
@@ -7,15 +7,7 @@ function IncludeLuaShared()
 	files(_GARRYSMOD_COMMON_DIRECTORY .. "/source/LuaShared.cpp")
 	vpaths({["Source files/garrysmod_common"] = _GARRYSMOD_COMMON_DIRECTORY .. "/source/LuaShared.cpp"})
 
-	filter("system:windows")
-		libdirs(_GARRYSMOD_COMMON_DIRECTORY .. "/lib/windows")
-
-	filter("system:linux")
-		libdirs(_GARRYSMOD_COMMON_DIRECTORY .. "/lib/linux")
-		links("dl")
-
-	filter("system:macosx")
-		libdirs(_GARRYSMOD_COMMON_DIRECTORY .. "/lib/macosx")
+	filter("system:linux or macosx")
 		links("dl")
 
 	filter({})
