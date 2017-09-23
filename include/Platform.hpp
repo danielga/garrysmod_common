86785d39e971c59c2539b2cd259f4abc7a974cfa
Daniel <danielga@outlook.pt>
2017-09-23 03:19:27 +0100

    Renamed some macros and defined them as 1 to conform with SourceSDK.
    Updated detouring submodule.

--- include/Platform.hpp
+++ include/Platform.hpp
@@ -2,15 +2,15 @@
 
 #if defined __clang__
 
-#define COMPILER_CLANG
+#define COMPILER_CLANG 1
 
 #elif defined __GNUC__
 
-#define COMPILER_GCC
+#define COMPILER_GNUC 1
 
 #elif defined _MSC_VER
 
-#define COMPILER_VS
+#define COMPILER_VC 1
 
 #else
 
@@ -20,23 +20,23 @@
 
 #if defined _WIN32
 
-#define SYSTEM_WINDOWS
+#define SYSTEM_WINDOWS 1
 
 #elif defined __linux
 
-#define SYSTEM_LINUX
-#define SYSTEM_POSIX
+#define SYSTEM_LINUX 1
+#define SYSTEM_POSIX 1
 
 #elif defined __APPLE__
 
-#define SYSTEM_MACOSX
-#define SYSTEM_POSIX
+#define SYSTEM_MACOSX 1
+#define SYSTEM_POSIX 1
 
 #include <AvailabilityMacros.h>
 
 #if MAC_OS_X_VERSION_MIN_REQUIRED < 1090
 
-#define SYSTEM_MACOSX_BAD
+#define SYSTEM_MACOSX_BAD 1
 
 #endif
 
@@ -48,11 +48,11 @@
 
 #if defined _M_IX86 || defined ___i386__ || defined __i386 || defined __X86__ || defined _X86_ || defined __I86__
 
-#define ARCHITECTURE_X86
+#define ARCHITECTURE_X86 1
 
 #elif defined _M_X64 || defined __amd64__ || defined __amd64 || defined __x86_64__ || defined __x86_64
 
-#define ARCHITECTURE_X86_64
+#define ARCHITECTURE_X86_64 1
 
 #else
 
