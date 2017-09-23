b6323bebd0bf3e3d5bbd8102818e726804cf6724
Daniel <danielga@outlook.pt>
2017-09-23 01:13:17 +0100

    Hopefully improved detection of compilers.
    Updated detouring and scanning submodules.

--- source/ABICompatibility.cpp
+++ source/ABICompatibility.cpp
@@ -1,8 +1,8 @@
-#if defined _WIN32 && _MSC_VER < 1910
+#if defined _WIN32 && ( _MSC_VER < 1910 || _MSC_VER > 1999 )
 
 #error The only supported compilation platform for this project on Windows is Visual Studio 2017 (for ABI compatibility reasons).
 
-#elif defined __linux && (__GNUC__ != 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 4))
+#elif defined __linux && ( __GNUC__ != 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ < 4 ) )
 
 #error The only supported compilation platforms for this project on Linux are GCC 4.4 to 4.9 (for ABI compatibility reasons).
 
@@ -10,7 +10,7 @@
 
 #include <AvailabilityMacros.h>
 
-#if MAC_OS_X_VERSION_MIN_REQUIRED > 1050
+#if MAC_OS_X_VERSION_MIN_REQUIRED != 1050
 
 #error The only supported compilation platform for this project on Mac OS X is GCC with Mac OS X 10.5 SDK (for ABI compatibility reasons).
 
