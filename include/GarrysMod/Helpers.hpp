bc0743006242ee2b9ba99cb5a90cff334020e4e3
Daniel <danielga@outlook.pt>
2018-07-08 23:47:50 +0100

    Added wide string version for GetBinaryFileName

--- include/GarrysMod/Helpers.hpp
+++ include/GarrysMod/Helpers.hpp
@@ -33,5 +33,33 @@ inline std::string GetBinaryFileName(
 #endif
 
 }
+    
+inline std::wstring GetWideBinaryFileName(
+    const std::wstring &name,
+    bool libprefix = true,
+    bool srvsuffix = true,
+    const std::wstring &extraprefix = L""
+)
+{
+    
+#if defined SYSTEM_WINDOWS
+    
+    (void)libprefix;
+    (void)srvsuffix;
+    return extraprefix + name + L".dll";
+    
+#elif defined SYSTEM_LINUX
+    
+    return extraprefix + ( libprefix ? L"lib" : L"" ) + name + ( srvsuffix ? L"_srv.so" : L".so" );
+    
+#elif defined SYSTEM_MACOSX
+    
+    (void)libprefix;
+    (void)srvsuffix;
+    return extraprefix + name + L".dylib";
+    
+#endif
+    
+}
 
 }
