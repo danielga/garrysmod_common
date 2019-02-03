b9ec70774310ab2db44cc6d9cf080cb1a9e67441
Daniel <danielga@outlook.pt>
2019-02-03 23:41:00 +0000

    Minimize repetition and improve error message

--- include/GarrysMod/Lua/Interface.h
+++ include/GarrysMod/Lua/Interface.h
@@ -5,26 +5,16 @@
 
 struct lua_State
 {
-#if defined( _WIN32 ) && !defined( _M_X64 )
-    // Win32
+#if ( defined( _WIN32 ) || defined( __linux__ ) || defined( __APPLE__ ) ) && \
+    !defined( __x86_64__ ) && !defined( _M_X64 )
+    // Win32, Linux32 and macOS32
     unsigned char _ignore_this_common_lua_header_[48 + 22];
-#elif defined( _WIN32 ) && defined( _M_X64 )
-    // Win64
-    unsigned char _ignore_this_common_lua_header_[92 + 22];
-#elif defined( __linux__ ) && !defined( __x86_64__ )
-    // Linux32
-    unsigned char _ignore_this_common_lua_header_[48 + 22];
-#elif defined( __linux__ ) && defined( __x86_64__ )
-    // Linux64
-    unsigned char _ignore_this_common_lua_header_[92 + 22];
-#elif defined ( __APPLE__ ) && !defined( __x86_64__ )
-    // macOS32
-    unsigned char _ignore_this_common_lua_header_[48 + 22];
-#elif defined ( __APPLE__ ) && defined( __x86_64__ )
-    // macOS64 (not tested)
+#elif ( defined( _WIN32 ) || defined( __linux__ ) || defined( __APPLE__ ) ) && \
+    ( defined( __x86_64__ ) || defined( _M_X64 ) )
+    // Win64, Linux64 and macOS64 (not tested)
     unsigned char _ignore_this_common_lua_header_[92 + 22];
 #else
-    #error agh
+    #error Unsupported platform
 #endif
 
     GarrysMod::Lua::ILuaBase* luabase;
