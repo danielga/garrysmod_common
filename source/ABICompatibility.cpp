c5edcd5729517619495880d336ccaa576cfef4e7
Daniel <danielga@outlook.pt>
2019-07-28 18:48:26 +0100

    Support unified Visual Studio runtime on ABI compatibility

--- source/ABICompatibility.cpp
+++ source/ABICompatibility.cpp
@@ -1,7 +1,8 @@
 #include <Platform.hpp>
 
-#if defined SYSTEM_WINDOWS && ( _MSC_VER < 1910 || _MSC_VER > 1999 )
+// Visual Studio 2015 to Visual Studio 2019
+#if defined SYSTEM_WINDOWS && ( _MSC_VER < 1900 || _MSC_VER > 1929 )
 
-#error The only supported compilation platform for this project on Windows is Visual Studio 2017 (for ABI compatibility reasons).
+#error The only supported compilation platforms for this project on Windows are Visual Studio 2015, 2017 and 2019 (for ABI compatibility reasons).
 
 #endif
