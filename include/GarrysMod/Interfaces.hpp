db4189fbd43d3e281151711a617658fc6e6596ca
Daniel <danielga@outlook.pt>
2019-10-06 15:04:25 +0100

    Add backwards compatibility fix to Interfaces.hpp

--- include/GarrysMod/Interfaces.hpp
+++ include/GarrysMod/Interfaces.hpp
@@ -3,3 +3,4 @@
 #pragma message( "GarrysMod/Interfaces.hpp: Deprecated header, please include GarrysMod/FactoryLoader.hpp instead." )
 
 #include "FactoryLoader.hpp"
+#include "Helpers.hpp" // for backwards compatibility
