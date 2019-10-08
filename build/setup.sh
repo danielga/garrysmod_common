8c86988de3332fcd79c1597fee9d22178a8f32b7
Daniel <danielga@outlook.pt>
2019-10-08 17:48:45 +0100

    Fix Travis CI builds failures

--- build/setup.sh
+++ build/setup.sh
@@ -22,17 +22,17 @@ validate_variable_or_set_default "SOURCE_SDK_BRANCH" "master"
 
 case "$(uname -s)" in
     Linux*)
-        validate_variable_or_set_default "PREMAKE5_URL" "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-linux.tar.gz";
-        validate_variable_or_set_default "PROJECT_OS" "linux";
-        ;
+        validate_variable_or_set_default "PREMAKE5_URL" "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-linux.tar.gz"
+        validate_variable_or_set_default "PROJECT_OS" "linux"
+        ;;
     Darwin*)
-        validate_variable_or_set_default "PREMAKE5_URL" "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-macosx.tar.gz";
-        validate_variable_or_set_default "PROJECT_OS" "osx";
-        ;
+        validate_variable_or_set_default "PREMAKE5_URL" "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha14/premake-5.0.0-alpha14-macosx.tar.gz"
+        validate_variable_or_set_default "PROJECT_OS" "osx"
+        ;;
     *)
-        echo "Unknown operating system";
-        exit 1;
-        ;
+        echo "Unknown operating system"
+        exit 1
+        ;;
 esac
 
 validate_variable_or_set_default "TARGET_OS" "$PROJECT_OS"
