63e7bacf29236f721afcc69ff7030f142a7c7524
Daniel <danielga@outlook.pt>
2019-10-20 12:48:51 +0100

    Use path.join for all paths

--- premake/lua_shared.lua
+++ premake/lua_shared.lua
@@ -14,23 +14,23 @@ function IncludeLuaShared()
 		location(path.join(_GARRYSMOD_COMMON_DIRECTORY, "projects", os.target(), _ACTION))
 		sysincludedirs(path.join(_GARRYSMOD_COMMON_DIRECTORY, "include"))
 		files({
-			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include/*.h"),
-			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include/*.hpp"),
-			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include/GarrysMod/**.h"),
-			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include/GarrysMod/**.hpp"),
-			path.join(_GARRYSMOD_COMMON_DIRECTORY, "source/LuaShared.cpp"),
-			path.join(_GARRYSMOD_COMMON_DIRECTORY, "source/ModuleLoader.cpp")
+			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include", "*.h"),
+			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include", "*.hpp"),
+			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include", "GarrysMod", "**.h"),
+			path.join(_GARRYSMOD_COMMON_DIRECTORY, "include", "GarrysMod", "**.hpp"),
+			path.join(_GARRYSMOD_COMMON_DIRECTORY, "source", "LuaShared.cpp"),
+			path.join(_GARRYSMOD_COMMON_DIRECTORY, "source", "ModuleLoader.cpp")
 		})
 		vpaths({
 			["Header files/*"] = {
-				path.join(_GARRYSMOD_COMMON_DIRECTORY, "include/**.h"),
-				path.join(_GARRYSMOD_COMMON_DIRECTORY, "include/**.hpp")
+				path.join(_GARRYSMOD_COMMON_DIRECTORY, "include", "**.h"),
+				path.join(_GARRYSMOD_COMMON_DIRECTORY, "include", "**.hpp")
 			},
-			["Source files/*"] = path.join(_GARRYSMOD_COMMON_DIRECTORY, "source/**.cpp")
+			["Source files/*"] = path.join(_GARRYSMOD_COMMON_DIRECTORY, "source", "**.cpp")
 		})
-		targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
+		targetdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		debugdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		objdir(path.join("!%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}", "intermediate", "%{prj.name}"))
 
 		filter("system:linux or macosx")
 			links("dl")
--- premake/sourcesdk.lua
+++ premake/sourcesdk.lua
@@ -27,19 +27,19 @@ local function IncludeSDKCommonInternal(directory)
 
 	if _project.serverside then
 		sysincludedirs({
-			path.join(directory, "game/server"),
-			path.join(directory, "game/shared")
+			path.join(directory, "game", "server"),
+			path.join(directory, "game", "shared")
 		})
 	else
 		sysincludedirs({
-			path.join(directory, "game/client"),
-			path.join(directory, "game/shared")
+			path.join(directory, "game", "client"),
+			path.join(directory, "game", "shared")
 		})
 	end
 
 	filter("system:windows")
 		defines("WIN32")
-		libdirs(path.join(directory, "lib/public"))
+		libdirs(path.join(directory, "lib", "public"))
 
 		filter({"system:windows", "configurations:Debug"})
 			linkoptions("/NODEFAULTLIB:\"libcmt\"")
@@ -53,7 +53,7 @@ local function IncludeSDKCommonInternal(directory)
 			"invalid-offsetof"
 		})
 		defines({"COMPILER_GCC", "POSIX", "_POSIX", "LINUX", "_LINUX", "GNUC", "NO_MALLOC_OVERRIDE"})
-		libdirs(path.join(path.getabsolute(directory), "lib/public/linux32"))
+		libdirs(path.join(path.getabsolute(directory), "lib", "public", "linux32"))
 
 	filter("system:macosx")
 		disablewarnings({
@@ -67,7 +67,7 @@ local function IncludeSDKCommonInternal(directory)
 			"invalid-offsetof"
 		})
 		defines({"COMPILER_GCC", "POSIX", "_POSIX", "OSX", "GNUC", "NO_MALLOC_OVERRIDE"})
-		libdirs(path.join(path.getabsolute(directory), "lib/public/osx32"))
+		libdirs(path.join(path.getabsolute(directory), "lib", "public", "osx32"))
 
 	filter({})
 end
@@ -86,7 +86,7 @@ function IncludeSDKTier0(directory)
 
 	directory = GetSDKPath(directory)
 
-	sysincludedirs(path.join(directory, "public/tier0"))
+	sysincludedirs(path.join(directory, "public", "tier0"))
 
 	filter("system:windows or macosx")
 		links("tier0")
@@ -105,7 +105,7 @@ function IncludeSDKTier1(directory)
 
 	directory = GetSDKPath(directory)
 
-	sysincludedirs(path.join(directory, "public/tier1"))
+	sysincludedirs(path.join(directory, "public", "tier1"))
 	links("tier1")
 
 	filter("system:windows")
@@ -125,69 +125,69 @@ function IncludeSDKTier1(directory)
 		location(path.join(_GARRYSMOD_COMMON_DIRECTORY, "projects", os.target(), _ACTION))
 		defines({"TIER1_STATIC_LIB", "_CRT_SECURE_NO_WARNINGS"})
 		sysincludedirs({
-			path.join(directory, "public/tier0"),
-			path.join(directory, "public/tier1")
+			path.join(directory, "public", "tier0"),
+			path.join(directory, "public", "tier1")
 		})
 		vpaths({["Source files/*"] = {
-			path.join(directory, "tier1/*.cpp"),
-			path.join(directory, "utils/lzma/C/*.c")
+			path.join(directory, "tier1", "*.cpp"),
+			path.join(directory, "utils", "lzma", "C", "*.c")
 		}})
 		IncludeSDKCommonInternal(directory)
 		files({
-			path.join(directory, "tier1/bitbuf.cpp"),
-			path.join(directory, "tier1/byteswap.cpp"),
-			path.join(directory, "tier1/characterset.cpp"),
-			path.join(directory, "tier1/checksum_crc.cpp"),
-			path.join(directory, "tier1/checksum_md5.cpp"),
-			path.join(directory, "tier1/checksum_sha1.cpp"),
-			path.join(directory, "tier1/commandbuffer.cpp"),
-			path.join(directory, "tier1/convar.cpp"),
-			path.join(directory, "tier1/datamanager.cpp"),
-			path.join(directory, "tier1/diff.cpp"),
-			path.join(directory, "tier1/generichash.cpp"),
-			path.join(directory, "tier1/ilocalize.cpp"),
-			path.join(directory, "tier1/interface.cpp"),
-			path.join(directory, "tier1/KeyValues.cpp"),
-			path.join(directory, "tier1/kvpacker.cpp"),
-			path.join(directory, "tier1/lzmaDecoder.cpp"),
-			path.join(directory, "tier1/mempool.cpp"),
-			path.join(directory, "tier1/memstack.cpp"),
-			path.join(directory, "tier1/NetAdr.cpp"),
-			path.join(directory, "tier1/splitstring.cpp"),
-			path.join(directory, "tier1/rangecheckedvar.cpp"),
-			path.join(directory, "tier1/reliabletimer.cpp"),
-			path.join(directory, "tier1/stringpool.cpp"),
-			path.join(directory, "tier1/strtools.cpp"),
-			path.join(directory, "tier1/strtools_unicode.cpp"),
-			path.join(directory, "tier1/tier1.cpp"),
-			path.join(directory, "tier1/tokenreader.cpp"),
-			path.join(directory, "tier1/sparsematrix.cpp"),
-			path.join(directory, "tier1/uniqueid.cpp"),
-			path.join(directory, "tier1/utlbuffer.cpp"),
-			path.join(directory, "tier1/utlbufferutil.cpp"),
-			path.join(directory, "tier1/utlstring.cpp"),
-			path.join(directory, "tier1/utlsymbol.cpp"),
-			path.join(directory, "tier1/utlbinaryblock.cpp"),
-			path.join(directory, "tier1/snappy.cpp"),
-			path.join(directory, "tier1/snappy-sinksource.cpp"),
-			path.join(directory, "tier1/snappy-stubs-internal.cpp"),
-			path.join(directory, "utils/lzma/C/LzmaDec.c")
+			path.join(directory, "tier1", "bitbuf.cpp"),
+			path.join(directory, "tier1", "byteswap.cpp"),
+			path.join(directory, "tier1", "characterset.cpp"),
+			path.join(directory, "tier1", "checksum_crc.cpp"),
+			path.join(directory, "tier1", "checksum_md5.cpp"),
+			path.join(directory, "tier1", "checksum_sha1.cpp"),
+			path.join(directory, "tier1", "commandbuffer.cpp"),
+			path.join(directory, "tier1", "convar.cpp"),
+			path.join(directory, "tier1", "datamanager.cpp"),
+			path.join(directory, "tier1", "diff.cpp"),
+			path.join(directory, "tier1", "generichash.cpp"),
+			path.join(directory, "tier1", "ilocalize.cpp"),
+			path.join(directory, "tier1", "interface.cpp"),
+			path.join(directory, "tier1", "KeyValues.cpp"),
+			path.join(directory, "tier1", "kvpacker.cpp"),
+			path.join(directory, "tier1", "lzmaDecoder.cpp"),
+			path.join(directory, "tier1", "mempool.cpp"),
+			path.join(directory, "tier1", "memstack.cpp"),
+			path.join(directory, "tier1", "NetAdr.cpp"),
+			path.join(directory, "tier1", "splitstring.cpp"),
+			path.join(directory, "tier1", "rangecheckedvar.cpp"),
+			path.join(directory, "tier1", "reliabletimer.cpp"),
+			path.join(directory, "tier1", "stringpool.cpp"),
+			path.join(directory, "tier1", "strtools.cpp"),
+			path.join(directory, "tier1", "strtools_unicode.cpp"),
+			path.join(directory, "tier1", "tier1.cpp"),
+			path.join(directory, "tier1", "tokenreader.cpp"),
+			path.join(directory, "tier1", "sparsematrix.cpp"),
+			path.join(directory, "tier1", "uniqueid.cpp"),
+			path.join(directory, "tier1", "utlbuffer.cpp"),
+			path.join(directory, "tier1", "utlbufferutil.cpp"),
+			path.join(directory, "tier1", "utlstring.cpp"),
+			path.join(directory, "tier1", "utlsymbol.cpp"),
+			path.join(directory, "tier1", "utlbinaryblock.cpp"),
+			path.join(directory, "tier1", "snappy.cpp"),
+			path.join(directory, "tier1", "snappy-sinksource.cpp"),
+			path.join(directory, "tier1", "snappy-stubs-internal.cpp"),
+			path.join(directory, "utils", "lzma", "C", "LzmaDec.c")
 		})
-		targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
+		targetdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		debugdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		objdir(path.join("!%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}", "intermediate", "%{prj.name}"))
 
 		filter("system:windows")
 			defines({"_DLL_EXT=.dll", "WIN32"})
-			files(path.join(directory, "tier1/processor_detect.cpp"))
+			files(path.join(directory, "tier1", "processor_detect.cpp"))
 
 		filter("system:linux")
 			disablewarnings("unused-result")
 			defines({"_DLL_EXT=.so", "COMPILER_GCC", "POSIX", "_POSIX", "LINUX", "_LINUX", "GNUC", "NO_MALLOC_OVERRIDE"})
 			files({
-				path.join(directory, "tier1/processor_detect_linux.cpp"),
-				path.join(directory, "tier1/qsort_s.cpp"),
-				path.join(directory, "tier1/pathmatch.cpp")
+				path.join(directory, "tier1", "processor_detect_linux.cpp"),
+				path.join(directory, "tier1", "qsort_s.cpp"),
+				path.join(directory, "tier1", "pathmatch.cpp")
 			})
 			linkoptions({
 				"-Xlinker --wrap=fopen",
@@ -224,7 +224,7 @@ function IncludeSDKTier1(directory)
 
 		filter("system:macosx")
 			defines({"_DLL_EXT=.dylib", "COMPILER_GCC", "POSIX", "_POSIX", "OSX", "GNUC", "NO_MALLOC_OVERRIDE"})
-			files(path.join(directory, "tier1/processor_detect_linux.cpp"))
+			files(path.join(directory, "tier1", "processor_detect_linux.cpp"))
 
 	group("")
 
@@ -236,16 +236,16 @@ function IncludeSDKTier2(directory)
 
 	directory = GetSDKPath(directory)
 
-	sysincludedirs(path.join(directory, "public/tier2"))
+	sysincludedirs(path.join(directory, "public", "tier2"))
 
 	filter("system:windows")
 		links("tier2")
 
 	filter("system:macosx")
-		linkoptions(path.join(path.getabsolute(directory), "lib/public/osx32/tier2.a"))
+		linkoptions(path.join(path.getabsolute(directory), "lib", "public", "osx32", "tier2.a"))
 
 	filter("system:linux")
-		linkoptions(path.join(path.getabsolute(directory), "lib/public/linux32/tier2.a"))
+		linkoptions(path.join(path.getabsolute(directory), "lib", "public", "linux32", "tier2.a"))
 
 	filter({})
 end
@@ -255,16 +255,16 @@ function IncludeSDKTier3(directory)
 
 	directory = GetSDKPath(directory)
 
-	sysincludedirs(path.join(directory, "public/tier3"))
+	sysincludedirs(path.join(directory, "public", "tier3"))
 
 	filter("system:windows")
 		links("tier3")
 
 	filter("system:macosx")
-		linkoptions(path.join(path.getabsolute(directory), "lib/public/osx32/tier3.a"))
+		linkoptions(path.join(path.getabsolute(directory), "lib", "public", "osx32", "tier3.a"))
 
 	filter("system:linux")
-		linkoptions(path.join(path.getabsolute(directory), "lib/public/linux32/tier3.a"))
+		linkoptions(path.join(path.getabsolute(directory), "lib", "public", "linux32", "tier3.a"))
 
 	filter({})
 end
@@ -277,7 +277,7 @@ function IncludeSDKMathlib(directory)
 
 	directory = GetSDKPath(directory)
 
-	sysincludedirs(path.join(directory, "public/mathlib"))
+	sysincludedirs(path.join(directory, "public", "mathlib"))
 	links("mathlib")
 
 	group("garrysmod_common")
@@ -288,40 +288,40 @@ function IncludeSDKMathlib(directory)
 		location(path.join(_GARRYSMOD_COMMON_DIRECTORY, "projects", os.target(), _ACTION))
 		defines("MATHLIB_LIB")
 		sysincludedirs({
-			path.join(directory, "public/mathlib"),
-			path.join(directory, "public/tier0"),
+			path.join(directory, "public", "mathlib"),
+			path.join(directory, "public", "tier0"),
 		})
-		vpaths({["Source files/*"] = path.join(directory, "mathlib/*.cpp")})
+		vpaths({["Source files/*"] = path.join(directory, "mathlib", "*.cpp")})
 		IncludeSDKCommonInternal(directory)
 		files({
-			path.join(directory, "mathlib/color_conversion.cpp"),
-			path.join(directory, "mathlib/halton.cpp"),
-			path.join(directory, "mathlib/lightdesc.cpp"),
-			path.join(directory, "mathlib/mathlib_base.cpp"),
-			path.join(directory, "mathlib/powsse.cpp"),
-			path.join(directory, "mathlib/sparse_convolution_noise.cpp"),
-			path.join(directory, "mathlib/sseconst.cpp"),
-			path.join(directory, "mathlib/sse.cpp"),
-			path.join(directory, "mathlib/ssenoise.cpp"),
-			path.join(directory, "mathlib/anorms.cpp"),
-			path.join(directory, "mathlib/bumpvects.cpp"),
-			path.join(directory, "mathlib/IceKey.cpp"),
-			path.join(directory, "mathlib/imagequant.cpp"),
-			path.join(directory, "mathlib/polyhedron.cpp"),
-			path.join(directory, "mathlib/quantize.cpp"),
-			path.join(directory, "mathlib/randsse.cpp"),
-			path.join(directory, "mathlib/spherical.cpp"),
-			path.join(directory, "mathlib/simdvectormatrix.cpp"),
-			path.join(directory, "mathlib/vector.cpp"),
-			path.join(directory, "mathlib/vmatrix.cpp"),
-			path.join(directory, "mathlib/almostequal.cpp")
+			path.join(directory, "mathlib", "color_conversion.cpp"),
+			path.join(directory, "mathlib", "halton.cpp"),
+			path.join(directory, "mathlib", "lightdesc.cpp"),
+			path.join(directory, "mathlib", "mathlib_base.cpp"),
+			path.join(directory, "mathlib", "powsse.cpp"),
+			path.join(directory, "mathlib", "sparse_convolution_noise.cpp"),
+			path.join(directory, "mathlib", "sseconst.cpp"),
+			path.join(directory, "mathlib", "sse.cpp"),
+			path.join(directory, "mathlib", "ssenoise.cpp"),
+			path.join(directory, "mathlib", "anorms.cpp"),
+			path.join(directory, "mathlib", "bumpvects.cpp"),
+			path.join(directory, "mathlib", "IceKey.cpp"),
+			path.join(directory, "mathlib", "imagequant.cpp"),
+			path.join(directory, "mathlib", "polyhedron.cpp"),
+			path.join(directory, "mathlib", "quantize.cpp"),
+			path.join(directory, "mathlib", "randsse.cpp"),
+			path.join(directory, "mathlib", "spherical.cpp"),
+			path.join(directory, "mathlib", "simdvectormatrix.cpp"),
+			path.join(directory, "mathlib", "vector.cpp"),
+			path.join(directory, "mathlib", "vmatrix.cpp"),
+			path.join(directory, "mathlib", "almostequal.cpp")
 		})
-		targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
+		targetdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		debugdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		objdir(path.join("!%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}", "intermediate", "%{prj.name}"))
 
 		filter("system:windows or linux")
-			files(path.join(directory, "mathlib/3dnow.cpp"))
+			files(path.join(directory, "mathlib", "3dnow.cpp"))
 
 		filter("system:windows")
 			defines("WIN32")
@@ -355,20 +355,20 @@ function IncludeSDKRaytrace(directory)
 		warnings("Default")
 		location(path.join(_GARRYSMOD_COMMON_DIRECTORY, "projects", os.target(), _ACTION))
 		sysincludedirs({
-			path.join(directory, "utils/common"),
-			path.join(directory, "public/tier0"),
-			path.join(directory, "public/tier1"),
+			path.join(directory, "utils", "common"),
+			path.join(directory, "public", "tier0"),
+			path.join(directory, "public", "tier1"),
 		})
-		vpaths({["Source files/*"] = path.join(directory, "raytrace/*.cpp")})
+		vpaths({["Source files/*"] = path.join(directory, "raytrace", "*.cpp")})
 		IncludeSDKCommonInternal(directory)
 		files({
-			path.join(directory, "raytrace/raytrace.cpp"),
-			path.join(directory, "raytrace/trace2.cpp"),
-			path.join(directory, "raytrace/trace3.cpp")
+			path.join(directory, "raytrace", "raytrace.cpp"),
+			path.join(directory, "raytrace", "trace2.cpp"),
+			path.join(directory, "raytrace", "trace3.cpp")
 		})
-		targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
+		targetdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		debugdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		objdir(path.join("!%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}", "intermediate", "%{prj.name}"))
 
 		filter("system:windows")
 			defines("WIN32")
@@ -401,29 +401,29 @@ function IncludeSDKBitmap(directory)
 		warnings("Default")
 		location(path.join(_GARRYSMOD_COMMON_DIRECTORY, "projects", os.target(), _ACTION))
 		includedirs({
-			path.join(directory, "utils/common"),
-			path.join(directory, "public/tier0"),
-			path.join(directory, "public/tier1")
+			path.join(directory, "utils", "common"),
+			path.join(directory, "public", "tier0"),
+			path.join(directory, "public", "tier1")
 		})
-		vpaths({["Source files/*"] = path.join(directory, "bitmap/*.cpp")})
+		vpaths({["Source files/*"] = path.join(directory, "bitmap", "*.cpp")})
 		IncludeSDKCommonInternal(directory)
 		files({
-			path.join(directory, "bitmap/colorconversion.cpp"),
-			path.join(directory, "bitmap/float_bm_bilateral_filter.cpp"),
-			path.join(directory, "bitmap/float_bm.cpp"),
-			path.join(directory, "bitmap/float_bm2.cpp"),
-			path.join(directory, "bitmap/float_bm3.cpp"),
-			path.join(directory, "bitmap/float_bm4.cpp"),
-			path.join(directory, "bitmap/float_cube.cpp"),
-			path.join(directory, "bitmap/imageformat.cpp"),
-			path.join(directory, "bitmap/psd.cpp"),
-			path.join(directory, "bitmap/resample.cpp"),
-			path.join(directory, "bitmap/tgaloader.cpp"),
-			path.join(directory, "bitmap/tgawriter.cpp")
+			path.join(directory, "bitmap", "colorconversion.cpp"),
+			path.join(directory, "bitmap", "float_bm_bilateral_filter.cpp"),
+			path.join(directory, "bitmap", "float_bm.cpp"),
+			path.join(directory, "bitmap", "float_bm2.cpp"),
+			path.join(directory, "bitmap", "float_bm3.cpp"),
+			path.join(directory, "bitmap", "float_bm4.cpp"),
+			path.join(directory, "bitmap", "float_cube.cpp"),
+			path.join(directory, "bitmap", "imageformat.cpp"),
+			path.join(directory, "bitmap", "psd.cpp"),
+			path.join(directory, "bitmap", "resample.cpp"),
+			path.join(directory, "bitmap", "tgaloader.cpp"),
+			path.join(directory, "bitmap", "tgawriter.cpp")
 		})
-		targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
+		targetdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		debugdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		objdir(path.join("!%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}", "intermediate", "%{prj.name}"))
 
 		filter("system:windows")
 			defines("WIN32")
@@ -462,21 +462,21 @@ function IncludeSDKVTF(directory)
 		links("bitmap")
 		location(path.join(_GARRYSMOD_COMMON_DIRECTORY, "projects", os.target(), _ACTION))
 		includedirs({
-			path.join(directory, "utils/common"),
-			path.join(directory, "public/tier0"),
-			path.join(directory, "public/tier1")
+			path.join(directory, "utils", "common"),
+			path.join(directory, "public", "tier0"),
+			path.join(directory, "public", "tier1")
 		})
-		vpaths({["Source files/*"] = path.join(directory, "vtf/*.cpp")})
+		vpaths({["Source files/*"] = path.join(directory, "vtf", "*.cpp")})
 		IncludeSDKCommonInternal(directory)
 		files({
-			path.join(directory, "vtf/cvtf.h"),
-			path.join(directory, "vtf/vtf.cpp"),
-			path.join(directory, "vtf/s3tc_decode.h"),
-			path.join(directory, "vtf/s3tc_decode.cpp")
+			path.join(directory, "vtf", "cvtf.h"),
+			path.join(directory, "vtf", "vtf.cpp"),
+			path.join(directory, "vtf", "s3tc_decode.h"),
+			path.join(directory, "vtf", "s3tc_decode.cpp")
 		})
-		targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
-		objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
+		targetdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		debugdir(path.join("%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}"))
+		objdir(path.join("!%{prj.location}", "%{cfg.architecture}", "%{cfg.buildcfg}", "intermediate", "%{prj.name}"))
 
 		filter("system:windows")
 			defines("WIN32")
@@ -504,7 +504,7 @@ function IncludeSteamAPI(directory)
 
 	directory = GetSDKPath(directory)
 
-	sysincludedirs(path.join(directory, "public/steam"))
+	sysincludedirs(path.join(directory, "public", "steam"))
 
 	links("steam_api")
 end
