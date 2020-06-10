46bb6a13fff02804fffccd620566d55465054100
Daniel <danielga@outlook.pt>
2020-06-10 02:04:07 +0100

    Add symbols for commonly used functions/interfaces

--- helpers_extended/source/FunctionPointers.cpp
+++ helpers_extended/source/FunctionPointers.cpp
@@ -16,15 +16,15 @@ namespace FunctionPointers
 
 	static SymbolFinder symbol_finder;
 
-	HandleClientLuaError_t HandleClientLuaError( )
+	CBasePlayer_HandleClientLuaError_t CBasePlayer_HandleClientLuaError( )
 	{
-		static HandleClientLuaError_t func_pointer = nullptr;
+		static CBasePlayer_HandleClientLuaError_t func_pointer = nullptr;
 		if( func_pointer != nullptr )
 			return func_pointer;
 
-		for( const auto &symbol : Symbols::HandleClientLuaError )
+		for( const auto &symbol : Symbols::CBasePlayer_HandleClientLuaError )
 		{
-			func_pointer = reinterpret_cast<HandleClientLuaError_t>( symbol_finder.Resolve(
+			func_pointer = reinterpret_cast<CBasePlayer_HandleClientLuaError_t>( symbol_finder.Resolve(
 				server_loader.GetModule( ), symbol.name.c_str( ), symbol.length
 			) );
 			if( func_pointer != nullptr )
@@ -44,6 +44,113 @@ namespace FunctionPointers
 		{
 			func_pointer = reinterpret_cast<CreateInterfaceFn>( symbol_finder.Resolve(
 				dedicated_loader.GetModule( ),
+				symbol.name.c_str( ),
+				symbol.length
+			) );
+			if( func_pointer != nullptr )
+				break;
+		}
+
+		return func_pointer;
+	}
+
+	CNetChan_ProcessMessages_t CNetChan_ProcessMessages( )
+	{
+		static CNetChan_ProcessMessages_t func_pointer = nullptr;
+		if( func_pointer != nullptr )
+			return func_pointer;
+
+		for( const auto &symbol : Symbols::CNetChan_ProcessMessages )
+		{
+			func_pointer = reinterpret_cast<CNetChan_ProcessMessages_t>( symbol_finder.Resolve(
+				engine_loader.GetModule( ),
+				symbol.name.c_str( ),
+				symbol.length
+			) );
+			if( func_pointer != nullptr )
+				break;
+		}
+
+		return func_pointer;
+	}
+
+	CBaseClient_ConnectionStart_t CBaseClient_ConnectionStart( )
+	{
+		static CBaseClient_ConnectionStart_t func_pointer = nullptr;
+		if( func_pointer != nullptr )
+			return func_pointer;
+
+		for( const auto &symbol : Symbols::CBaseClient_ConnectionStart )
+		{
+			func_pointer = reinterpret_cast<CBaseClient_ConnectionStart_t>( symbol_finder.Resolve(
+				engine_loader.GetModule( ),
+				symbol.name.c_str( ),
+				symbol.length
+			) );
+			if( func_pointer != nullptr )
+				break;
+		}
+
+		return func_pointer;
+	}
+
+	CBaseClientState_ConnectionStart_t CBaseClientState_ConnectionStart( )
+	{
+		static CBaseClientState_ConnectionStart_t func_pointer = nullptr;
+		if( func_pointer != nullptr )
+			return func_pointer;
+
+		const auto starting_point = reinterpret_cast<const uint8_t *>( CBaseClient_ConnectionStart( ) );
+		for( const auto &symbol : Symbols::CBaseClientState_ConnectionStart )
+		{
+			func_pointer = reinterpret_cast<CBaseClientState_ConnectionStart_t>( symbol_finder.Resolve(
+				engine_loader.GetModule( ),
+				symbol.name.c_str( ),
+				symbol.length,
+				// starting point for sigscan
+				// we use an offset because, on Linux, CBaseClient::ConnectionStart and
+				// CBaseClientState::ConnectionStart have the same signature
+				// this code expects CBaseClient::ConnectionStart to appear before
+				// CBaseClientState::ConnectionStart
+				starting_point + 16
+			) );
+			if( func_pointer != nullptr )
+				break;
+		}
+
+		return func_pointer;
+	}
+
+	CBaseServer_RecalculateTags_t CBaseServer_RecalculateTags( )
+	{
+		static CBaseServer_RecalculateTags_t func_pointer = nullptr;
+		if( func_pointer != nullptr )
+			return func_pointer;
+
+		for( const auto &symbol : Symbols::CBaseServer_RecalculateTags )
+		{
+			func_pointer = reinterpret_cast<CBaseServer_RecalculateTags_t>( symbol_finder.Resolve(
+				engine_loader.GetModule( ),
+				symbol.name.c_str( ),
+				symbol.length
+			) );
+			if( func_pointer != nullptr )
+				break;
+		}
+
+		return func_pointer;
+	}
+
+	GModDataPack_SendFileToClient_t GModDataPack_SendFileToClient( )
+	{
+		static GModDataPack_SendFileToClient_t func_pointer = nullptr;
+		if( func_pointer != nullptr )
+			return func_pointer;
+
+		for( const auto &symbol : Symbols::GModDataPack_SendFileToClient )
+		{
+			func_pointer = reinterpret_cast<GModDataPack_SendFileToClient_t>( symbol_finder.Resolve(
+				server_loader.GetModule( ),
 				symbol.name.c_str( ),
 				symbol.length
 			) );
