93e8525e26bb3f5e97a8295ddb43a64590dc21ad
Daniel <danielga@outlook.pt>
2020-06-14 17:15:16 +0100

    Fix links for helpers_extended

--- helpers_extended/premake5.lua
+++ helpers_extended/premake5.lua
@@ -6,7 +6,9 @@ function IncludeHelpersExtended()
 	local _project = project()
 
 	sysincludedirs(current_dir .. "/include")
-	links("helpers_extended")
+	-- GCC requires manual ordering of links, where libraries on the left depend on libraries on the right
+	-- Since helpers_extended depends on helpers, the latter must appear after
+	links({"helpers_extended", "helpers"})
 
 	if refcount == 1 then
 		include(current_dir .. "/premake5_create_project.lua")
