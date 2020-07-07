1d0c7262e1b5ae0b7f1bc04a5847244b08bcdb62
Daniel <danielga@outlook.pt>
2020-07-07 21:33:45 +0100

    Allow multiple runs of project creator files
    Do package reference counting from workspace and not globally

--- helpers/premake5.lua
+++ helpers/premake5.lua
@@ -9,7 +9,7 @@ function IncludeHelpers()
 	links("helpers")
 
 	if refcount == 1 then
-		include(current_dir .. "/premake5_create_project.lua")
+		dofile(current_dir .. "/premake5_create_project.lua")
 		project(_project.name)
 	end
 end
--- lua_shared/premake5.lua
+++ lua_shared/premake5.lua
@@ -8,7 +8,7 @@ function IncludeLuaShared()
 	links("lua_shared")
 
 	if refcount == 1 then
-		include(current_dir .. "/premake5_create_project.lua")
+		dofile(current_dir .. "/premake5_create_project.lua")
 		project(_project.name)
 	end
 end
